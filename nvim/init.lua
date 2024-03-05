e7c3e4aa9e552525d4214b38d6531ca4e3919c26
OToL <barchange@yahoo.fr>
2024-03-05 12:04:47 +0100

    drop loading of plugins_index

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,2 @@
-require("ebeau.core")           -- load base global utilities, settings and keymaps
-require("ebeau.plugins_index")  -- register plugins to enable
+require("ebeau.core")           -- load base global utilities, settings and keymapsq
 require("ebeau.lazy")           -- kickstart plugins loading & initialization
